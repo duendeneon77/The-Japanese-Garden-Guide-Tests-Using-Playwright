429974dc640a3c0229eb2e5be116dc3e161b8e22
Arthur Henrique <duendeneon@hotmail.com>
2026-07-11 23:39:19 -0300

    fixing teardown

--- tests/global-teardown.js
+++ tests/global-teardown.js
@@ -8,7 +8,8 @@ import { cleanUpVideosByVideosPrefix } from './helpers/videosHelper.js';
 import { cleanUpHistoryPage } from './helpers/historyPageHelper.js';
 import { cleanUpProjectPage } from './helpers/projectPageHelper.js';
 import { cleanUpWaterSessionPage } from './helpers/editWaterSessionHelper.js';
-import { cleanUpBridgesSessionPage, cleanUpToroSessionPage } from './helpers/editBridgesSessionHelper.js';
+import {cleanUpToroSessionPage} from './helpers/editToroSessionHelper.js'
+import { cleanUpBridgesSessionPage } from './helpers/editBridgesSessionHelper.js';
 import { cleanUpRocksSessionPage } from './helpers/editRocksSessionHelper.js';
 
 export default async function globalTeardown() {
@@ -18,14 +19,23 @@ export default async function globalTeardown() {
   await doLogin(page);
 
   await cleanUpSpeciesByPrefix(page, 'Pinheiro Branco');
+  console.log('clean species')
   await cleanUpArtclesByArticlePrefix(page,'Test-Article')
+  console.log('clean up articles')
   await cleanUpVideosByVideosPrefix(page,'Test-Video')
+  console.log('clean up videos')
   await cleanUpHistoryPage(page)
+  console.log('clean up history')
   await cleanUpProjectPage(page)
+  console.log('clean up project')
   await cleanUpWaterSessionPage(page)
+  console.log('clean up waterses')
   await cleanUpToroSessionPage(page)
+  console.log('clean up toroses')
   await cleanUpBridgesSessionPage(page)
+  console.log('clean up bridgesses')
   await cleanUpRocksSessionPage(page)
+  console.log('clean up rocksses')
 
   await browser.close();
 
